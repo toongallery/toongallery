cbb9a90426f7937919b3f4a87fce9895def8e939
dlwjdals3667@gmail.com <dlwjdals3667@gmail.com>
2025-03-24 17:41:38 +0900

    feat:기본 틀 생성

--- src/main/java/com/example/toongallery/config/JwtAuthenticationToken.java
+++ src/main/java/com/example/toongallery/config/JwtAuthenticationToken.java
@@ -1,4 +1,25 @@
 package com.example.toongallery.config;
 
-public class JwtAuthenticationToken {
+import com.example.toongallery.domain.common.dto.AuthUser;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final AuthUser authUser;
+
+    public JwtAuthenticationToken(AuthUser authUser) {
+        super(authUser.getAuthorities());
+        this.authUser = authUser;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return authUser;
+    }
 }
--- src/main/java/com/example/toongallery/domain/common/dto/AuthUser.java
+++ src/main/java/com/example/toongallery/domain/common/dto/AuthUser.java
@@ -1,7 +1,11 @@
-package com.example.toongallery.domain.dto;
+package com.example.toongallery.domain.common.dto;
 
+import com.example.toongallery.domain.user.enums.UserRole;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.Collection;
 import java.util.List;
 
 @Getter
