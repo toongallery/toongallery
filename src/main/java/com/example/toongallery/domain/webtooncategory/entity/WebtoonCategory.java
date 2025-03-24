462fa18c4233193ddfdab982079df6e509b937a0
lamgak12 <tntnqk12@naver.com>
2025-03-24 20:51:07 +0900

    feat: 담당 기능 엔티티 추가
    ERD상의 엔티티 대로 Category, Episode, Image, WebtoonCategory 엔티티 로직 추가

---
+++ src/main/java/com/example/toongallery/domain/webtooncategory/entity/WebtoonCategory.java
@@ -0,0 +1,25 @@
+package com.example.toongallery.domain.webtooncategory.entity;
+
+import com.example.toongallery.domain.category.entity.Category;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "webtooncategory")
+public class WebtoonCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "webtoon_id", nullable = false)
+//    private Webtoon webtoon;
+}
